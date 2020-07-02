0a541b88c4fe411d95ecd4fec1292eb09a0dcfc3
Milad Darani <57505176+miladDarani@users.noreply.github.com>
2020-07-02 18:27:49 -0500

    QUIZ2 added database info

---
+++ quiz2/database/migrations/2020_07_02_230320_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('student_number')->unsigned();
+            $table->string('first_name', 255) ;
+            $table->string('last_name', 255) ;
+            $table->string('image',255) ;
+            $table->string('email', 255) ;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
