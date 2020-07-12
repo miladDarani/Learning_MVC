844d32fe7a0a5c0ef20427efb02038ba6cc13cd4
Milad Darani <57505176+miladDarani@users.noreply.github.com>
2020-07-12 09:05:19 -0500

    server issue

--- blog/config/filesystems.php
+++ blog/config/filesystems.php
@@ -80,6 +80,8 @@ return [
 
     'links' => [
         public_path('images') => storage_path('app/images'),
+        public_path('public/storage') => storage_path('storage/app/public'),
+
     ],
 
 ];
